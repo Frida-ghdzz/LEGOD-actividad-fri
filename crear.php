1acf94dab8b8039d33afe371139da43ef4512d63
Garcia Hernandez Frida <325295949@alumno.enp.unam.mx>
2026-06-04 15:54:00 -0600

    feat(crear):Añadi modificacion para añadir set

--- crear.php
+++ crear.php
@@ -1,5 +1,18 @@
 <?php
+include 'config.php';
 
+$sql = "SELECT theme_id, name FROM themes";
+$squery = mysqli_query($conexion, $sql);
+$lista_temas = array();
+
+if($squery){
+    while($fila = mysqli_fetch_assoc($squery)){
+        $lista_temas[] = $fila;
+        //var_dump($fila);
+        //echo "<br>"; echo "<br>";
+    }
+}
+var_dump($lista_temas);
 ?>
 
 <!DOCTYPE html>
@@ -44,7 +57,12 @@
                     
                     <!-- PHP FOREACH --> 
                     <?php
-                    
+                    if(count($lista_temas) > 0 ) {
+                        foreach($lista_temas as tema){
+                            $id_tema $tema["theme_id"];
+                            echo "<option value =' $id_tema>" . $tema[name] . "</echo>"
+                        }
+                    }
                     ?>
                 </select>
                 
